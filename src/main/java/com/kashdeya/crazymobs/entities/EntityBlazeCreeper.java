8cd2bc76ff578a1a67af1ff84733c8f1935306d1
kashdeya <kashdeya@outlook.com>
2018-01-14 20:48:05 -0600

    new ai for blaze creeper

--- src/main/java/com/kashdeya/crazymobs/entities/EntityBlazeCreeper.java
+++ src/main/java/com/kashdeya/crazymobs/entities/EntityBlazeCreeper.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityBlaze;
 import net.minecraft.entity.player.EntityPlayer;
@@ -40,12 +41,12 @@ public class EntityBlazeCreeper extends EntityBlaze {
 	  protected void initEntityAI()
 	    {
 	        this.tasks.addTask(4, new EntityBlazeCreeper.AIFireballAttack(this));
-	        this.tasks.addTask(3, new EntityAIMoveTowardsRestriction(this, 2.0D));
-	        this.tasks.addTask(6, new EntityAIWander(this, 0.8D));
-	        this.tasks.addTask(7, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-	        this.tasks.addTask(7, new EntityAILookIdle(this));
-	        this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
-	        this.targetTasks.addTask(2, new EntityAIHurtByTarget(this, false, new Class[0]));
+	        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 2.0D));
+	        this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D, 0.0F));
+	        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+	        this.tasks.addTask(8, new EntityAILookIdle(this));
+	        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false, new Class[0]));
+	        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
 	    }
 	  
 	  @Override
